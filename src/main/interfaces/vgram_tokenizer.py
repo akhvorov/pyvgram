539bb63edd2abde7b541abc910664112e998c7f7
Aleksandr Khvorov <khvorov.aleksandr@gmail.com>
2020-12-14 22:01:17 +0300

    Migrate tokenizer implementation to VgramApplier

--- src/main/interfaces/vgram_tokenizer.py
+++ src/main/interfaces/vgram_tokenizer.py
@@ -1,38 +1,23 @@
 import json
 import random
-from typing import List, Union, Optional
+from typing import List, Union
 
-from main.algorithm.int_dictionary import IntDictionary
-from main.algorithm.vgram_builder import VGramBuilder
+from main.interfaces.vgram_applier import IterativeVGramApplier, StaticVGramApplier
+from main.interfaces.base_tokenizer import BaseTokenizer
 from main.interfaces.coder import SimpleCoder
-from main.interfaces.tokenizer import Tokenizer
 
 
-class VGramTokenizer(Tokenizer):
-    def __init__(self, size: int = 30000, words_level: bool = True):
+class VGramTokenizer(BaseTokenizer):
+    def __init__(self, size: int = 30000, words_level: bool = True, verbose: bool = False):
+        super().__init__(words_level)
         self.coder = SimpleCoder()
-        self.size = size
-        self.words_level = words_level
-        self.dict: Optional[IntDictionary] = None
-
-    def _split_words(self, text: str) -> List[str]:
-        if self.words_level:
-            words = []
-            word = ""
-            for c in text:
-                if not c.isalnum():
-                    words.append(word)
-                    word = ""
-                word += c
-            words.append(word)
-        else:
-            words = [text]
-        return words
+        self.verbose = verbose
+        self.vgram_applier = IterativeVGramApplier(size, self.verbose)
 
     def _encode_one(self, seq: str) -> List[int]:
         coded = []
         for word in self._split_words(seq):
-            coded += self.dict.parse(self.coder.encode(word))
+            coded += self.vgram_applier.parse(self.coder.encode(word))
         return coded
 
     def encode(self, seqs: Union[str, List[str]]) -> Union[List[int], List[List[int]]]:
@@ -43,7 +28,7 @@ class VGramTokenizer(Tokenizer):
     def tokenize(self, seqs: Union[str, List[str]]) -> Union[List[str], List[List[str]]]:
         def tokenize_one(seq: str) -> List[str]:
             coded = self._encode_one(seq)
-            return [self.coder.decode(self.dict.get(id)) for id in coded]
+            return [self.coder.decode(self.vgram_applier.get(id)) for id in coded]
 
         if type(seqs) is str:
             return tokenize_one(seqs)
@@ -51,7 +36,7 @@ class VGramTokenizer(Tokenizer):
 
     def decode(self, coded_seqs: Union[int, List[int], List[List[int]]]) -> Union[str, List[str]]:
         def decode_one(seq: List[int]) -> str:
-            return "".join([self.coder.decode(self.dict.get(id)) for id in seq])
+            return "".join([self.coder.decode(self.vgram_applier.get(id)) for id in seq])
 
         if type(coded_seqs) is int:
             return decode_one([coded_seqs])
@@ -61,8 +46,7 @@ class VGramTokenizer(Tokenizer):
             return decode_one(coded_seqs)
         return [decode_one(seq) for seq in coded_seqs]
 
-    def fit(self, texts: Union[str, List[str]], iters: int = 1, verbose: int = 0):
-        vgram_builder = VGramBuilder(self.size, verbose)
+    def fit(self, texts: Union[str, List[str]], iters: int = 1):
         if type(texts) is str:
             texts = [texts]
         for iter in range(iters):
@@ -70,12 +54,12 @@ class VGramTokenizer(Tokenizer):
                 line = texts[random.randint(0, len(texts) - 1)]
                 for word in self._split_words(line):
                     ids = self.coder.encode(word)
-                    vgram_builder.accept(ids)
+                    self.vgram_applier.accept(ids)
 
-        self.dict = vgram_builder.result()
+        self.coder.fix()
+        self.vgram_applier.update()
 
-    def train(self, files: Union[str, List[str]], iters: int = 1, verbose: int = 0):
-        vgram_builder = VGramBuilder(self.size, verbose)
+    def train(self, files: Union[str, List[str]], iters: int = 1):
         if type(files) is str:
             files = [files]
         for iter in range(iters):
@@ -87,26 +71,26 @@ class VGramTokenizer(Tokenizer):
                         line = lines[i].strip()
                         for word in self._split_words(line):
                             ids = self.coder.encode(word)
-                            vgram_builder.accept(ids)
+                            self.vgram_applier.accept(ids)
 
         self.coder.fix()
-        self.dict = vgram_builder.result()
+        self.vgram_applier.update()
 
     def get_vocab(self) -> List[str]:
-        return [self.coder.decode(list(seq)) for seq in self.dict.alphabet()]
+        return [self.coder.decode(list(seq)) for seq in self.vgram_applier.dict.alphabet()]
 
     def vocab_size(self) -> int:
-        return self.dict.size()
+        return self.vgram_applier.dict.size()
 
     def save_pretrained(self, path: str):
-        res = {"dict": self.dict.to_json(), "coder": self.coder.to_json(),
-               "words_level": self.words_level, "size": self.size}
+        res = {"applier": self.vgram_applier.to_json(), "coder": self.coder.to_json(),
+               "words_level": self.words_level}
         json.dump(res, open(path, 'w'))
 
     @staticmethod
     def from_pretrained(path: str) -> 'VGramTokenizer':
         res = json.load(open(path))
-        tokenizer = VGramTokenizer(res["size"], res["words_level"])
-        tokenizer.dict = IntDictionary.from_json(res["dict"])
+        tokenizer = VGramTokenizer(res["words_level"])
+        tokenizer.vgram_applier = StaticVGramApplier.from_json(res["applier"])
         tokenizer.coder = SimpleCoder.from_json(res["coder"])
         return tokenizer
